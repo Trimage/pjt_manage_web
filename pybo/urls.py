4ea91e7d41865bcc74ad2d25a317ce34482400f6
200603_1 <seokwon0127@naver.com>
2020-06-04 17:45:31 +0900

    index.html 모든 버튼 urls.py 및 view내 연결 처리 완료 *로그아웃, 검색, 레포트저장 등의 기능을 별도로 손봐야함

--- pybo/urls.py
+++ pybo/urls.py
@@ -7,4 +7,18 @@ app_name = 'pybo'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:question_id>/', views.detail, name='detail'),
+    path('charts/', views.charts, name='charts'),
+    path('tables/', views.tables, name='tables'),
+    path('buttons/', views.buttons, name='buttons'),
+    path('cards/', views.cards, name='cards'),
+    path('utilities-color/', views.utilities_colors, name='utilities-colors'),
+    path('utilities-border/', views.utilities_borders, name='utilities-borders'),
+    path('utilities-animation/', views.utilities_animations, name='utilities-animations'),
+    path('utilities-other/', views.utilities_other, name='utilities-other'),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
+    path('forgot-password/', views.forgot_password, name='forgot-password'),
+    path('page404/', views.page404, name='page404'),
+    path('blank/', views.blank, name='blank'),
+
 ]
