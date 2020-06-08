3b6cf6e74b7b4f1df6b87c51e72ac6786fa33c6f
200603_1 <seokwon0127@naver.com>
2020-06-08 13:58:41 +0900

    1. views.py 내 detail 함수 수정(게시글 접속안되는 경우 404페이지로 가기)

--- pybo/views.py
+++ pybo/views.py
@@ -131,8 +131,13 @@ def detail(request, question_id):
     """
     pybo 내용 출력
     """
-    question = get_object_or_404(Question, pk=question_id)
+    try :
+        question = get_object_or_404(Question, pk=question_id)
+    except :
+        return render(request, 'pybo/404.html')
+
     context = {'question': question}
+
     return render(request, 'pybo/question_detail.html', context)
 
 
